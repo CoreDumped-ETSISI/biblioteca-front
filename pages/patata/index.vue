81cdeccaa814ed11b067839108766c255818ab8e
Core Dumped UPM <coredumped@etsisi.upm.es>
2019-04-26 11:38:59 +0200

    Added submit button that saves files in the APIs temp_file

--- pages/patata/index.vue
+++ pages/patata/index.vue
@@ -2,5 +2,9 @@
     <div>
     <h1>Me comes la patata</h1>
     <p>Y si no te apetece, me da igual. Te la vas a comer de todas formas.</p>
+    <form action="http://localhost:3003/book/uploadBook" method="post" enctype="multipart/form-data">
+        <input type="file" name="book" />
+        <input type="submit" value="Upload" />
+    </form>
     </div>
 </template>
