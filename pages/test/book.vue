0854f98a90442785b2a5c12436e4891044fecd27
Néstor Javier Oubiña Arriaga <nestoroa@hotmail.com>
2019-05-31 18:40:29 +0200

    Book and catalog components
    You can check on /test/book/

---
+++ pages/test/book.vue
@@ -0,0 +1,17 @@
+<template>
+    <catalog></catalog>
+</template>
+
+<script>
+import catalog from '~/components/catalog/BookCatalog.vue'
+
+export default {
+  components: {
+    catalog
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
