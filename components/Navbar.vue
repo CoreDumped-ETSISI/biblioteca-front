de6ff7d1f85b89a25c4ede6867f31625849107fd
luiscal4a <luiscal4a@gmail.com>
2020-01-06 03:47:52 +0100

    Changed navbar title

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <b-navbar toggleable="lg" type="dark" variant="success">
-    <b-navbar-brand href="/">NavBar</b-navbar-brand>
+    <b-navbar-brand href="/">Coreteca</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
